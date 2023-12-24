e924a6d664bb6832a69a32693799b4b4d4fd796a
pearhp <leehuynhphuc@gmail.com>
2023-12-24 17:44:00 +0700

    firse commit

--- carhub/settings.py
+++ carhub/settings.py
@@ -13,9 +13,9 @@ import os
 import dj_database_url
 import django_heroku
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
